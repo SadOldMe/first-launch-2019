a97775f841e7ba20c44e156d680fae40e249dd6a
Brannon Penrod <43364611+brannon-penrod@users.noreply.github.com>
2019-01-24 07:52:57 -0800

    Update Robot_TalonCANExample.cpp
    Spacing and variable names.

--- Programming/Robot_TalonCANExample.cpp
+++ Programming/Robot_TalonCANExample.cpp
@@ -1,3 +1,4 @@
+//Files the program will pull definitions from
 #include <frc/Joystick.h>
 #include <frc/Spark.h>
 #include <frc/drive/DifferentialDrive.h>
@@ -6,22 +7,21 @@
 #include <ctre/Phoenix.h>
 #include <ctre/phoenix/motorcontrol/can/BaseMotorController.h>
 
+//so we don't have to put frc:: in front of everything
 using namespace frc;
 
 class Robot : public frc::IterativeRobot {
  public:
   void TeleopPeriodic() override {
-    tln_left.Set(mode, joy_xbox.GetY());
-    tln_right.Set(mode, -joy_xbox.GetY());
+   tln_left.Set(mode, joy_xbox.GetY());
+   tln_right.Set(mode, -joy_xbox.GetY());
   }
 
  private:
-  Joystick joy_xbox { 0 };
-
-  TalonSRX tln_right { 1 };
-  TalonSRX tln_left { 2 };
-
-  ControlMode mode;
+   Joystick joy_xbox { 0 };
+   TalonSRX mtr_talonRight { 1 };
+   TalonSRX mtr_talonLeft { 2 };
+   ControlMode mode;
 };
 
 #ifndef RUNNING_FRC_TESTS
