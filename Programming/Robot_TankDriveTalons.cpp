786a9fb31eb33fda76aaf41040678822e831d6c5
Braxten Chenay <46695819+braxten509@users.noreply.github.com>
2019-01-26 10:11:43 -0800

    Update and rename Arcadedrive_Talons.cpp to Robot_TankDriveTalons.cpp

---
+++ Programming/Robot_TankDriveTalons.cpp
@@ -0,0 +1,39 @@
+//Files the program will pull definitions from
+#include <frc/Joystick.h>
+#include <frc/Spark.h>
+#include <frc/drive/DifferentialDrive.h>
+#include <frc/IterativeRobot.h>
+#include <frc/talon.h>  
+#include <ctre/Phoenix.h>
+#include <ctre/phoenix/motorcontrol/can/BaseMotorController.h>
+#include <frc/RobotDrive.h>
+
+//so we don't have to put frc:: in front of everything
+using namespace frc;
+
+double speed = 1; 
+
+class Robot : public frc::IterativeRobot {
+ public:
+  void TeleopPeriodic() override {
+  
+    drv_wheels.TankDrive(joy_xbox.GetRawAxis(1) * speed, joy_xbox.GetRawAxis(5) * speed, true);
+    
+  }
+
+ private:
+
+    //DON'T FORGET SEMICOLONS HERE!!!
+
+    Joystick joy_xbox { 0 };
+    WPI_TalonSRX mtr_talonRight { 1 };
+    WPI_TalonSRX mtr_talonLeft { 2 };
+    ControlMode mode;
+
+    DifferentialDrive drv_wheels {mtr_talonRight, mtr_talonLeft};
+   
+};
+
+#ifndef RUNNING_FRC_TESTS
+int main() { return frc::StartRobot<Robot>(); }
+#endif
