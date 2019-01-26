c7433e0b257f3494620709126b5f24eab42201c8
Braxten Chenay <46695819+braxten509@users.noreply.github.com>
2019-01-25 19:36:11 -0800

    Update Robot_TalonCANExample.cpp

--- Programming/Robot_TalonCANExample.cpp
+++ Programming/Robot_TalonCANExample.cpp
@@ -6,22 +6,32 @@
 #include <frc/talon.h>
 #include <ctre/Phoenix.h>
 #include <ctre/phoenix/motorcontrol/can/BaseMotorController.h>
+#include <RobotDrive.h>
 
 //so we don't have to put frc:: in front of everything
 using namespace frc;
 
+//int speed = .20;
+
 class Robot : public frc::IterativeRobot {
  public:
   void TeleopPeriodic() override {
-   tln_left.Set(mode, joy_xbox.GetY());
-   tln_right.Set(mode, -joy_xbox.GetY());
+    //Chaotic Corner Drive
+    //mtr_talonLeft.Set(mode, joy_xbox.GetY()/2); 
+    //mtr_talonRight.Set(mode, joy_xbox.GetX()/2);
+
+    //Tank Drive
+    mtr_talonLeft.Set(mode, -joy_xbox.GetRawAxis(1)/1); 
+    mtr_talonRight.Set(mode, joy_xbox.GetRawAxis(5)/1);
+   
   }
 
  private:
-   Joystick joy_xbox { 0 };
-   TalonSRX mtr_talonRight { 1 };
-   TalonSRX mtr_talonLeft { 2 };
-   ControlMode mode;
+    Joystick joy_xbox { 0 };
+    TalonSRX mtr_talonRight { 1 };
+    TalonSRX mtr_talonLeft { 2 };
+    ControlMode mode;
+   
 };
 
 #ifndef RUNNING_FRC_TESTS
