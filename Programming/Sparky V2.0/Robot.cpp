0c134bf6fc6de42265c89fa5cc84cd249ea8860a
Brannon Penrod <43364611+brannon-penrod@users.noreply.github.com>
2019-01-31 22:43:45 -0800

    Update Robot.cpp
    Fixed acceleration drive (Still needs tweaks.)

--- Programming/Sparky V2.0/Robot.cpp
+++ Programming/Sparky V2.0/Robot.cpp
@@ -13,23 +13,27 @@ using namespace frc;
 
 // Variable declaration //
 
+// DRIVER //
 WPI_TalonSRX tln_leftWheel { 1 }; //left motor
 WPI_TalonSRX tln_rightWheel { 2 }; //right motor
-WPI_TalonSRX tln_arm { 3 }; //arm connected to Cargo In/Out box
-WPI_TalonSRX tln_cargoIO { 4 }; //axel + wheels used for intake and output of cargo balls
-
-DifferentialDrive drv_Wheels { tln_leftWheel, tln_rightWheel }; //left + right wheel diff. drive
 
 Encoder enc_leftWheel { 0, 1 }; //left wheel encoder
 Encoder enc_rightWheel { 2, 3 }; //right wheel encoder
-Encoder enc_arm { 4, 5 }; //arm encoder
 
-Joystick joy_xbox { 0 }; //xbox controller
+DifferentialDrive drv_Wheels { tln_leftWheel, tln_rightWheel }; //left + right wheel diff. drive
+double accelMult = 0.3; //used to modify acceleration multiplier for the wheels
 
-BuiltInAccelerometer accelerometer; //RoboRIO built-in accelerometer
+Joystick joy_driver { 0 }; //controller for main driver
+int driver_leftStickY = 1, driver_rightStickY = 3; //driver sticks
 
-double accelMult = 0.01; //used to modify acceleration multiplier on drive
+// CO-DRIVER //
+WPI_TalonSRX tln_arm { 3 }; //arm connected to Cargo In/Out box
+WPI_TalonSRX tln_cargoIO { 4 }; //axel + wheels used for intake and output of cargo balls
+
+Encoder enc_arm { 4, 5 }; //encoder for arm holding cargo bay
 const double pi = 3.141592653; //used for encoder distances
+
+Joystick joy_codriver { 1 }; //controller for co driver (Arm control)
 
 // End variable declaration //
 
@@ -37,8 +41,6 @@ void Robot::RobotInit() {
   m_chooser.SetDefaultOption(kAutoNameDefault, kAutoNameDefault);
   m_chooser.AddOption(kAutoNameCustom, kAutoNameCustom);
   SmartDashboard::PutData("Auto Modes", &m_chooser);
-
-  drv_Wheels.SetRightSideInverted(true);
 
   enc_rightWheel.SetSamplesToAverage(5);
   enc_rightWheel.SetDistancePerPulse(1.0 / 360.0 * 2.0 * pi * 1.5);
@@ -51,19 +53,17 @@ void Robot::RobotInit() {
   enc_arm.SetSamplesToAverage(5);
   enc_arm.SetDistancePerPulse(1.0 / 360.0 * 2.0 * pi * 1.5); //to do: find radius of wheel/motor/thing that moves the arm
   enc_arm.SetMinRate(1.0); 
-  }
+}
 
 void Robot::RobotPeriodic() {
-  if(accelMult < 1. && (joy_xbox.GetRawAxis(1) > 0.01 || joy_xbox.GetRawAxis(5) > 0.01)){
+  if(joy_driver.GetRawAxis(driver_leftStickY) > 0.1 || joy_driver.GetRawAxis(driver_rightStickY) > 0.1 || 
+     joy_driver.GetRawAxis(driver_leftStickY) < -0.1 || joy_driver.GetRawAxis(driver_rightStickY) < -0.01){ //the sticks were moved
     accelMult += 0.01;
   }
 
-  if(joy_xbox.GetRawAxis(1) < 0.1 && joy_xbox.GetRawAxis(5) < 0.1) { //the sticks were stopped being pushed forward
-    AutoBrake(drv_Wheels);
-    accelMult = 0.01;
+  else { //the sticks were stopped being pushed forward
+    accelMult = 0.3;
   }
-
-  SmartDashboard::PutNumber("Acceleration Multiplier", accelMult);
 }
 
 void Robot::AutonomousInit() {
@@ -72,8 +72,7 @@ void Robot::AutonomousInit() {
   if (m_autoSelected == kAutoNameCustom) {
     // Custom Auto goes here
   } else {
-    enc_leftWheel.Reset();
-    enc_rightWheel.Reset();
+    //Default Auto goes here
   }
 }
 
@@ -91,23 +90,13 @@ void Robot::AutonomousPeriodic() {
 void Robot::TeleopInit() {}
 
 void Robot::TeleopPeriodic() {
-  drv_Wheels.TankDrive(joy_xbox.GetRawAxis(1) * accelMult, joy_xbox.GetRawAxis(5) * accelMult);
+  drv_Wheels.TankDrive(joy_driver.GetRawAxis(driver_leftStickY) * accelMult, joy_driver.GetRawAxis(driver_rightStickY) * accelMult);
 }
 
 //Use these functions to test controls BEFORE putting them in periodic classes.
 void Robot::TestInit() {}
 
 void Robot::TestPeriodic() {}
-
-// User-defined functions //
-
-void Robot::AutoBrake(DifferentialDrive& drive) {
-  while(accelerometer.GetX() > 1.0 || accelerometer.GetX() < -1.0) {
-    drive.TankDrive(-accelerometer.GetX(), -accelerometer.GetX());
-  }
-}
-
-// End user-defined functions //
 
 #ifndef RUNNING_FRC_TESTS
 int main() { return StartRobot<Robot>(); }
