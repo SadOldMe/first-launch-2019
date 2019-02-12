2b15c1c65bd6e2d92e441ead9be0337e9f1bf90b
TheMarkVI <34019229+TheMarkVI@users.noreply.github.com>
2019-02-11 16:55:28 -0800

    Update Robot.cpp

--- Programming/Sparky V2.0/Robot.cpp
+++ Programming/Sparky V2.0/Robot.cpp
@@ -13,30 +13,40 @@ using namespace frc;
 // Variable declaration //
 
 // DRIVER //
-WPI_TalonSRX tln_leftWheel { 1 }; //left motor
-WPI_TalonSRX tln_rightWheel { 2 }; //right motor
+WPI_TalonSRX tln_leftFront { 1 }; //left front motor
+WPI_TalonSRX tln_leftBack { 2 }; //left back motor
+WPI_TalonSRX tln_rightFront { 3 }; // right front motor
+WPI_TalonSRX tln_rightBack { 4 }; //right back motor
 
 Encoder enc_leftWheel { 0, 1 }; //left wheel encoder
 Encoder enc_rightWheel { 2, 3 }; //right wheel encoder
 
-DifferentialDrive drv_Wheels { tln_leftWheel, tln_rightWheel }; //left + right wheel diff. drive
+DifferentialDrive drv_wheels { tln_leftFront, tln_rightFront }; //left + right wheel diff. drive
 double accelMult = 0.3; //used to modify acceleration multiplier for the wheels
+double accelMultMax = 0.75;
 
 Joystick joy_driver { 0 }; //controller for main driver
 int driver_leftStickY = 1, driver_rightStickY = 3; //driver sticks
 
 // CO-DRIVER //
-WPI_TalonSRX tln_arm { 3 }; //arm connected to Cargo In/Out box
-WPI_TalonSRX tln_cargoIO { 4 }; //axel + wheels used for intake and output of cargo balls
+WPI_TalonSRX tln_arm { 5 }; //arm connected to Cargo In/Out box
+//WPI_TalonSRX tln_cargoIO { 6 }; //axle + wheels used for intake and output of cargo balls
 
 Encoder enc_arm { 4, 5 }; //encoder for arm holding cargo bay
 const double pi = 3.141592653; //used for encoder distances
 
 Joystick joy_codriver { 1 }; //controller for co driver (Arm control)
+int co_stickY = 1;
 
 // End variable declaration //
 
 void Robot::RobotInit() {
+  tln_leftFront.SetInverted(true);
+  tln_rightFront.SetInverted(true);
+
+  tln_arm.SetSafetyEnabled(true);
+  tln_arm.SetNeutralMode(NeutralMode::Brake);
+
   m_chooser.SetDefaultOption(kAutoNameDefault, kAutoNameDefault);
   m_chooser.AddOption(kAutoNameCustom, kAutoNameCustom);
   SmartDashboard::PutData("Auto Modes", &m_chooser);
@@ -55,9 +65,14 @@ void Robot::RobotInit() {
 }
 
 void Robot::RobotPeriodic() {
-  if(joy_driver.GetRawAxis(driver_leftStickY) > 0.1 || joy_driver.GetRawAxis(driver_rightStickY) > 0.1 || 
-     joy_driver.GetRawAxis(driver_leftStickY) < -0.1 || joy_driver.GetRawAxis(driver_rightStickY) < -0.1){ //the sticks were moved
+  bool sticksMoving = (joy_driver.GetRawAxis(driver_leftStickY) > 0.1 || joy_driver.GetRawAxis(driver_rightStickY) > 0.1 || 
+     joy_driver.GetRawAxis(driver_leftStickY) < -0.1 || joy_driver.GetRawAxis(driver_rightStickY) < -0.1);
+  if( sticksMoving && accelMult < accelMultMax ) { //the sticks were moved
     accelMult += 0.01;
+  }
+
+  else if(sticksMoving && accelMult >= accelMultMax) {
+    accelMult = accelMultMax;
   }
 
   else { //the sticks were stopped being pushed forward
@@ -89,13 +104,16 @@ void Robot::AutonomousPeriodic() {
 void Robot::TeleopInit() {}
 
 void Robot::TeleopPeriodic() {
-  drv_Wheels.TankDrive(joy_driver.GetRawAxis(driver_leftStickY) * accelMult, joy_driver.GetRawAxis(driver_rightStickY) * accelMult);
+  drv_wheels.TankDrive(joy_driver.GetRawAxis(driver_leftStickY) * accelMult, joy_driver.GetRawAxis(driver_rightStickY) * accelMult);
+  tln_arm.Set(joy_codriver.GetRawAxis(co_stickY) * 0.25);
 }
 
 //Use these functions to test controls BEFORE putting them in periodic classes.
 void Robot::TestInit() {}
 
-void Robot::TestPeriodic() {}
+void Robot::TestPeriodic() {
+    drv_wheels.TankDrive(-1, -1);
+}
 
 #ifndef RUNNING_FRC_TESTS
 int main() { return StartRobot<Robot>(); }
